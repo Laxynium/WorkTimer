94ecc8c95af152a354b4b90e6162b27b78b1f8a6
Laxynium <grzesiu15432@gmail.com>
2023-08-07 07:59:46 +0200

    Take work time from args + display minutes + seconds

--- WorkTimer.Console/Program.cs
+++ WorkTimer.Console/Program.cs
@@ -2,8 +2,13 @@
 using System.Reactive.Linq;
 using Spectre.Console;
 
-var timeInMinutes = 1; //Convert.ToInt32(args[1]);
-var timeInSeconds = 10; //timeInMinutes * 60;
+var timeInMinutes = 90;
+if (args.Length >= 1)
+{
+    timeInMinutes = Convert.ToInt32(args[0]);
+}
+
+var timeInSeconds = timeInMinutes * 60;
 
 var countdownTimer = Observable
     .Timer(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(1))
@@ -11,16 +16,21 @@ var countdownTimer = Observable
     .ObserveOn(Scheduler.Default);
 
 
-var counterView = AnsiConsole.Live(new FigletText("90"))
+var counterView = AnsiConsole.Live(new FigletText("90").Centered().Color(Color.Blue))
     .StartAsync(async ctx =>
     {
         await countdownTimer
             .Select(x => x)
             .ForEachAsync(i =>
             {
-                ctx.UpdateTarget(new FigletText($"{timeInSeconds - i}"));
+                var minutes = (timeInSeconds - i) / 60;
+                var seconds = (timeInSeconds - i) % 60;
+                ctx.UpdateTarget(new FigletText($"{minutes:D2}:{seconds:D2}").Centered().Color(Color.Blue));
                 ctx.Refresh();
             });
     });
 
 await counterView;
+
+AnsiConsole.Clear();
+AnsiConsole.Write(new FigletText($"Your {timeInMinutes}m work time is up").Centered().Color(Color.Green3));
